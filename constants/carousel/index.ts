b9afdf6844ff2f17c32032bee6e95d6333652c2b
tuon1602 <tuan69878@gmail.com>
2024-04-06 12:31:05 +0700

    fixes hero image

--- constants/carousel/index.ts
+++ constants/carousel/index.ts
@@ -4,21 +4,21 @@ export const carouselItems = [
         name:'Chainsaw man',
         slug:"chainsaw-man-tho-san-quy",
         description:"Bộ manga được xem nhiều nhất năm 2023",
-        imageUrl:"/assets/images/carousel1.png",
+        imageUrl:"/assets/images/carousel1.webp",
         categorySlug:"action",
     },
     {
         name:"Spy x Family",
         slug:"gia-dinh-diep-vien",
         description:"Bộ manga được yêu thích nhất năm 2023",
-        imageUrl:"/assets/images/carousel2.png",
+        imageUrl:"/assets/images/carousel2.webp",
         categorySlug:"action",
     },
     {
         name:"Solo leveling",
         slug:"toi-thang-cap-mot-minh",
         description:"Bộ manga đang thịnh hành trong năm",
-        imageUrl:"/assets/images/carousel3.png",
+        imageUrl:"/assets/images/carousel3.webp",
         categorySlug:"action",
     }
 ]
